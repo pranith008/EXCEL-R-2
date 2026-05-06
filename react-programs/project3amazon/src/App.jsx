bcfd68639857f20e165bacad3e400075762566ed
Mamidi Pranith <mamidipranith008@gmail.com>
2026-05-06 19:41:49 +0530

    upload

---
+++ react-programs/project3amazon/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ShowAllProducts from './components/ShowAllProducts'
+import ShowAllProducts2 from './components/ShowAllProducts2'
+import ShowAllProducts3 from './components/ShowAllProducts3'
+
+const App = () => {
+  return (
+   <ShowAllProducts3/>
+  )
+}
+
+export default App
---
+++ react-programs/project3amazon/src/components/ShowAllProducts3.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import './ShowAllProducts.css';
+import productsx from '../data/allproducts';
+import './ShowAllProducts2.css'
+import Product from './Products';
+import FilterBar from './Filterbar';
+import { useState } from 'react';
+import FilterByCategoryBar from './FilterByCategoryBar';
+import FilterByPrice from './FilterByPrice';
+import FilterByPrice2 from './FilterByPrice2';
+
+const ShowAllProducts3 = () => {
+
+    const [products,setProducts]=useState(productsx)
+    const [buproducts,setBuproducts]=useState(productsx)
+
+    const productCategories=buproducts.map(
+        (prod)=>prod.category
+    )
+
+    console.log(productCategories)
+
+    const uniqueCategories=new Set(productCategories)
+    console.log(uniqueCategories)
+
+    const allCategories=[...uniqueCategories,'All']
+    console.log(allCategories)
+
+    function filterByCategory(userSelectedCategory)
+    {
+        if(userSelectedCategory==='All')
+        {
+            setProducts(buproducts)
+        }
+        else
+        {
+        //console.log(userSelectedCategory)
+        const filteredProducts=buproducts.filter(
+            (prod)=>prod.category===userSelectedCategory
+        )
+
+        //console.log(filteredProducts)
+        setProducts(filteredProducts)
+    }
+    }
+
+    function filterByPrice(basePrice)
+    {
+        const filteredProducts=buproducts.filter(
+            (prod)=>prod.price<=basePrice
+        )
+
+        setProducts(filteredProducts)
+    }
+
+    function filterByPriceNew(event)
+    {
+        console.log(event.target.value)
+        const filteredProducts=buproducts.filter(
+            (prod)=>prod.price<=event.target.value
+        )
+
+        setProducts(filteredProducts)
+    }
+    return (
+        <>
+        <FilterByCategoryBar categories={allCategories} handleClick={filterByCategory}/>
+        {/* <FilterByPrice handleClick={filterByPrice}/> */}
+        <FilterByPrice2 handleChange={filterByPriceNew}/>
+        <div className="container py-5">
+            <div className="row g-4">
+                {products.map((product, index) => {
+                    return <Product key={index} product={product}/>
+                })}
+            </div>
+        </div>
+        </>
+    );
+};
+
+export default ShowAllProducts3;
