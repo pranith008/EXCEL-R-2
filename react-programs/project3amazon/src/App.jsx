8b2296c15472a1342a2bc1685868a39ad3bf8591
Mamidi Pranith <mamidipranith008@gmail.com>
2026-05-08 19:51:32 +0530

    upload

--- react-programs/project3amazon/src/App.jsx
+++ react-programs/project3amazon/src/App.jsx
@@ -2,10 +2,26 @@ import React from 'react'
 import ShowAllProducts from './components/ShowAllProducts'
 import ShowAllProducts2 from './components/ShowAllProducts2'
 import ShowAllProducts3 from './components/ShowAllProducts3'
+import AllProductsPage from './pages/AllProductsPage'
+import RictTextBox2 from './pages/RictTextBox2'
+import AllProductsPage1 from './pages/AllProductsPage1'
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import NavBar from './components/NavBar'
+import LandingPage from './pages/LandingPage'
+import NavBar1 from './components/NavBar1'
+import SingleProduct from './pages/SingleProduct'
 
 const App = () => {
   return (
-   <ShowAllProducts3/>
+  <Router>
+      <NavBar1/>
+          <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/shop" element={<AllProductsPage1 />} />
+                <Route path="/rich" element={<RictTextBox2 />} />
+                <Route path="/single/:prodid" element={<SingleProduct />} />
+          </Routes>
+  </Router>
   )
 }
 
--- react-programs/project3amazon/src/components/FilterByPrice.jsx
+++ react-programs/project3amazon/src/components/FilterByPrice.jsx
@@ -10,5 +10,4 @@ const FilterByPrice = ({handleClick}) => {
    </ul>
   )
 }
-
 export default FilterByPrice
